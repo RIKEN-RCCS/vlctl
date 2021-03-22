b425ebe8247c9efcbc946eabdd425f8bee1718be
SUZUKI Soichiro <soichiro.suzuki@riken.jp>
2021-03-22 20:58:30 +0900

    Modify run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,9 @@
 #!/bin/bash -x
 #PJM --rsc-list "node=1"
-#PJM --rsc-list "rscunit=rscunit_ft02"
-#PJM --rsc-list "rscgrp=dv1-10"
+#PJM --rsc-list "rscunit=rscunit_ft01"
+#PJM --rsc-list "rscgrp=eap-small"
 #PJM --rsc-list "elapse=1:00"
 #PJM -S
-
-module load lang
 
 ./show_vl
 
